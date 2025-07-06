42ef7973000faa1ce6e83889ea9259e8e9d960e5
coxlaurel <coxlaurel@gmail.com>
2025-07-06 15:48:00 -0400

    more components

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,12 @@ import './index.css';
 import NavBar from './components/NavBar';
 import ProjectCard from './components/ProjectCard'
 import About from './components/About';
+import SocialLinks from './components/SocialLinks'
+import Footer from './components/Footer';
 import useScrollSpy from './hooks/useScrollSpy';
+import Cursor2 from './components/Cursor2';
+
+import { useState } from 'react';
 
 function App() {
   const {
@@ -11,23 +16,36 @@ function App() {
     experienceRef,
     projectsRef,
   } = useScrollSpy();
+
+  const [isActive, setIsActive] = useState(false);
   
   return <div>
+    <Cursor2 isActive={isActive}/>
     <div className='nav-content-container'>
-      <div className='nav-bar-container'>
-        <NavBar currentSection={currentSection} />
+      <div className='left-bar-container'>
+        <div>
+          <NavBar currentSection={currentSection} />
+        </div>
+        <SocialLinks />
       </div>
       <div className='content-container'>
         <section ref={aboutRef} id="about">
           <About />
         </section>
-        <section ref={experienceRef} id="experience" style={{height: "100vh"}}>
-          <h1>Experience Place holder</h1>
+        <section ref={experienceRef} id="experience"> {/*style={{height: "100vh"}} */}
+          <ProjectCard heading="TTM Technologies" cardType={["experience", "May 2025 - Present"]} caption="Overhauling existing 4.0 dashboard to a Unified Namespace structure to improve manufacturing scalability and capabilities." tags={["Jython", "CSS", "SQL", "Ignition"]} />
+          <ProjectCard heading="ORBITAL: Olin Assistive Tech Lab" cardType={["experience", "Jan 2025 - Present"]} caption="Delightful caption" tags={["Python", "Pillow Library", "BIBD"]} />
+          <ProjectCard heading="Vending Machine" cardType={["experience", "Jan 2025 - Present"]} caption="Table top vending machine with touchscreen interface to vend all your favorite snacks. Made as a final project for Principles of Engineering course." tags={["Jython", "CSS", "SQL"]} />
         </section>
-        <section ref={projectsRef} id="projects" style={{height: "100vh"}}>
-          <ProjectCard heading="Vending Machine" image="src/assets/Screenshot from 2024-06-25 19-14-40.png" caption="Snacking Delight"/>
-          <ProjectCard heading="Heading 2" image="src/assets/Screenshot from 2024-06-25 19-14-40.png" caption="Delightful caption"/>
+        <section ref={projectsRef} id="projects">
+          <ProjectCard heading="Vending Machine" cardType={["project", "src/assets/vend_thumbnail.png"]} caption="Table top vending machine with touchscreen interface to vend all your favorite snacks. Made as a final project for Principles of Engineering course." tags={["Python", "Arduino", "Motor circuits", "RasPI"]} />
+          <ProjectCard heading="Inkstory: Tattoo Museum" cardType={["project", "src/assets/inkstory_thumbnail.jpg"]} caption="Worked with 10+ tattoo artist to deeply understand their values and pain-points. Co-designed a mockup tattoo museum solution over the semester." tags={["Co-design", "User Research", "Stories"]}/>
+          <ProjectCard heading="Cloudy With A Chance Of Pizza" cardType={["project", "src/assets/CWACOP_thumbnail.png"]} caption="Make pizza to your hearts delight. Play with pixel perfect collision." tags={["Python", "Pygame Lib", "Game Dev"]} />
+          <ProjectCard heading="Spot-it: Discrete Math Version" cardType={["project", "src/assets/spot_it_thumbnail.png"]} caption="Delightful caption" tags={["Python", "Pillow Library", "BIBD"]} />
         </section>
+        <section>
+          <Footer />
+          </section>
       </div>
     </div>
   </div>
--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -2,22 +2,42 @@ import './styles/ProjectCard.css';
 
 interface Props {
     heading: string;
-    image: string;
+    cardType: string[];
     caption: string;
+    tags: string[];
 }
 
-function ProjectCard({heading, image, caption}: Props){
+function CardType({ cardType }: { cardType: string[] }) {
+    if (cardType[0] == "project") {
+        return <>
+            <div className="img-div">
+                <img src={cardType[1]} height="175px" width="175px" style={{borderRadius: "5px"}}/>
+            </div>
+        </>
+    } else {
+        return <>
+            <div className="date-div">
+                <p className='date-text'>{cardType[1]}</p>
+            </div>
+        </>
+    }
+}
+
+function ProjectCard({heading, cardType, caption, tags}: Props){
     return <>
         <div className='card-div'>
-            <div className="img-div">
-                <img src={image} height="200px" width="200px"/>
-            </div>
+            <CardType cardType={cardType} />
             <div className='content-div'>
-                <h1>{heading}</h1>
-                <p>{caption}</p>
+                <h1 className='card-content-head'>{heading}</h1>
+                <p className='card-content-body'>{caption}</p>
+                <div className="tags-container">
+                    {tags.map((tag) => (
+                        <p className='tag'>{tag}</p>
+                    ))}
+                </div>
             </div>
         </div>
     </>
 }
 
-export default ProjectCard
+export default ProjectCard;
